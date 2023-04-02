3f01a9a7977133e954fd0cd00b6fedeb58a22d12
depin1 <boymatobvnusratillo1@gmail.com>
2023-04-02 23:28:16 +0500

    main

--- files/main.js
+++ files/main.js
@@ -1,7 +1,7 @@
 let row = document.querySelector('.row');
-
 fetch('https://restcountries.com/v3.1/all').then((el) => el.json()).then((date) => {
     date.sort((a, b) => {
+
         if (a.name.common > b.name.common) {
             return 1;
         } else if (a.name.common < b.name.common) {
@@ -10,20 +10,24 @@ fetch('https://restcountries.com/v3.1/all').then((el) => el.json()).then((date) 
             return 0;
         }
     })
-    const mydata = date.map((country) => {
-        console.log(country);
+    let datamap = date.map((country) => {
+        let newp = country.population.toString();
+        let stringP = [];
+        if (newp.length <= 6) {
+            stringP = `${newp.slice(0,newp.length-2)/10}k`
+        } else if (newp.length <= 9) {
+            stringP = `${newp.slice(0,newp.length-5)/10}mln`
+        } else if (newp.length <= 12) {
+            stringP = `${newp.slice(0,newp.length-8)/10}b`
+        }
         return `
-        <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="card" style="width: 18rem;">
-                    <img src="${country.flags.png}" class="image card-img-top"  alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${country.name.common} </h5>
-                      <p class="card-text">${country.capital} </p>
-                      <a href="#" class="btn btn-primary">${country.population} </a>
-                    </div>
-                  </div>
-            </div> 
-    `
+  <div class="col-lg-3 col-md-6 col-sm-12">
+  <img class="card-img-top" src="${country.flags.png}" alt="">
+  <h2 class="name_country">${country.name.common}</h2>
+  <h2 class="name_cp">${country.capital}</h2>
+  <p class="population">${stringP}</p>
+</div>
+  `
     })
-    row.innerHTML = mydata.join('')
+    row.innerHTML = datamap.join('')
 })
